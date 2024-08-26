23e96a5d9607481c07d510cabf8af6c78672dfc7
IevaLiepa <167983954+IevaLiepa@users.noreply.github.com>
2024-08-26 19:50:29 +0300

    8/26

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
 import express from 'express';
+import { servicesRouter } from './router/servicesRouter.js';
+import { teamRouter } from './router/teamRouter.js';
+import { discountRouter } from './router/discountRouter.js';
+import { studentsRouter } from './router/studentsRouter.js';
+import { booksRouter } from './router/booksRouter.js';
+import { phonesRouter } from './router/phonesRouter.js';
+import { apiRouter } from './router/apiRouter.js';
 
 const app = express();
 const port = 3000;
 
+// for parsing application/json
+app.use(express.json({
+    type: 'application/json',
+}));
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    return res.send('Labas rytas, Lietuva!');
+    return res.send('Home page');
 });
 
 app.get('/about', (req, res) => {
-    return res.send('Nori suzinoti apie si projekta? 😏');
+    return res.send('About page');
 });
 
+app.get('/img', (req, res) => {
+    return res.send('Images...');
+});
+
+app.get('/img/logo.png', (req, res) => {
+    return res.send('Images: logo.png turinys :P');
+});
+
+app.use('/api', apiRouter);
+app.use('/services', servicesRouter);
+app.use('/team', teamRouter);
+app.use('/discount', discountRouter);
+app.use('/students', studentsRouter);
+app.use('/books', booksRouter);
+app.use('/phones', phonesRouter);
+
 app.get('*', (req, res) => {
-    return res.send('Ups... Norimas puslapis nerastas 🛸');
+    return res.send('Ups... 404 page 🛸');
 });
 
 app.listen(port, () => {
     console.log(`App running on: http://localhost:${port}`);
 });
+
+/*
+
+/students
+Mokosi 1 studentai: Ona.
+Mokosi 2 studentai: Petras ir Ona.
+Mokosi 3 studentai: Maryte, Petras ir Ona.
+Mokosi 4 studentai: Jonas, Maryte, Petras ir Ona.
+
+/students/jonas
+/students/Jonas
+/students/JoNas
+/students/JONAS
+Studentas, vardu Jonas yra 99 metu amziaus ir yra vedes.
+
+/students/chuck
+Studento, vardu chuck nera.
+
+/students/Chuck
+Studento, vardu Chuck nera.
+
+*/
